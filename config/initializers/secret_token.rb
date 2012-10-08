38e69161aad72d420c434578fb4ede99141dddc6
Aaron Cruz <aaron@aaroncruz.com>
2012-10-08 14:03:07 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tapatracker::Application.config.secret_token = '09679dd7eb8b900cdb294e56d356e44a7856b160c2587f97e7f05402736183ce13b5482421c85be536f9c8cb181a15ebf2e145e5696ea95687c5ea234206b491'
