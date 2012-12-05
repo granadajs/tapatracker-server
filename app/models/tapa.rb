a901d11724ac7a68a9b12dc141f15317c72f8e46
Aaron Cruz <aaron@aaroncruz.com>
2012-12-05 14:27:23 +0100

    Category works in form

--- app/models/tapa.rb
+++ app/models/tapa.rb
@@ -3,6 +3,14 @@ class Tapa < ActiveRecord::Base
   has_many :categorizations
   has_many :categories, through: :categorizations
 
+  def category=(name)
+    add_to_category name
+  end
+
+  def category
+    categories.last.name if categories.last
+  end
+
   def add_to_category name
     category = Category.find_or_create_by_name name
     category.tapas << tapa
